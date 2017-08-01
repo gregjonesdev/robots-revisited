e56485aedaf6d6501bb51c2a24e3687d03a36503
Greg Jones <gmjones@gmail.com>
2017-08-01 17:55:30 -0500

    basic styling

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.set('view engine', 'mustache');
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient
 const url = 'mongodb://localhost:27017/robot';
+app.use(express.static('public'))
 
 app.listen(3000);
 
@@ -24,6 +25,24 @@ app.get('/', function(req,res){
       .toArray( function(err, robots){
 
         res.render("index", {robots: robots})
+      })
+  })
+});
+
+app.get('/:id', function(req,res){
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      throw err;
+    } else {
+      console.log('Successfully connected to the database');
+    }
+
+    db.collection('robots')
+      .find()
+      .toArray( function(err, robots){
+
+        res.render("profile")
       })
   })
 });
