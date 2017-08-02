78fc8006f8ced8fb9883bd5629699bbc07a9aab6
Greg Jones <gmjones@gmail.com>
2017-08-01 19:00:05 -0500

    working

--- app.js
+++ app.js
@@ -11,8 +11,41 @@ app.use(express.static('public'))
 
 app.listen(3000);
 
-app.get('/', function(req,res){
+app.get('/working', function(req,res){
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      throw err;
+    } else {
+      console.log('Successfully connected to the database');
+    }
 
+    db.collection('robots')
+      .find()
+      .toArray( function(err, robots){
+
+        res.render("working", {robots: robots})
+      })
+  })
+})
+
+app.get('/notworking', function(req,res){
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      throw err;
+    } else {
+      console.log('Successfully connected to the database');
+    }
+
+    db.collection('robots')
+      .find()
+      .toArray( function(err, robots){
+
+        res.render("notworking", {robots: robots})
+      })
+  })
+})
+
+app.get('/', function(req,res){
   MongoClient.connect(url, function(err, db) {
     if (err) {
       throw err;
@@ -27,10 +60,11 @@ app.get('/', function(req,res){
         res.render("index", {robots: robots})
       })
   })
-});
+})
+
+
 
 app.get('/:id', function(req,res){
-
   MongoClient.connect(url, function(err, db) {
     if (err) {
       throw err;
@@ -38,11 +72,11 @@ app.get('/:id', function(req,res){
       console.log('Successfully connected to the database');
     }
 
-    db.collection('robots')
-      .find()
-      .toArray( function(err, robots){
+  db.collection('robots')
+    .find({"id": parseInt(req.params.id)})
+    .toArray( function(err, robots){
 
-        res.render("profile")
-      })
-  })
-});
+      res.render("id", {robots: robots})
+    })
+})
+})
